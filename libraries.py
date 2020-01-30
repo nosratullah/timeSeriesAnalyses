58307c3207e774dd7ac865b23f621a2aa49600b7
Nosratullah Mohammadi <nosratullah.mohammadi@gmail.com>
2020-01-30 11:40:16 +0330

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -2,6 +2,7 @@ from scipy.signal import hilbert,find_peaks,correlate
 import numpy as np
 from statsmodels.tsa.stattools import acf,ccf
 
+# A function to extract peaks
 def extract_peaks(data):
     amps = np.abs(hilbert(data))
     peaks_times = find_peaks(amps)
@@ -12,6 +13,7 @@ def extract_peaks(data):
     peaks = peaks[:-1]
     return peaks
 
+# A function to calculate the IEI (Interevent intervals)
 def time_diffrences(data):
     amps = np.abs(hilbert(data))
     peaks_times = find_peaks(amps)
@@ -21,6 +23,7 @@ def time_diffrences(data):
 
     return diff_list
 
+# A function to extract peaks 
 def amps_detection(data):
     amps_data = np.abs(hilbert(data))
     amps_times = find_peaks(amps_data)
@@ -32,17 +35,19 @@ def amps_detection(data):
     amps_peak_values = np.array(amps_peak_values)
     return amps_peak_values
 
+# A function to shuffle the data and then finding the cross correlation between thos two data
 def shuff_corr(data1, data2):
     np.random.shuffle(data1)
     np.random.shuffle(data2)
     shuff_corr = ccf(data1,data2,unbiased=False)
     return shuff_corr
-
+# A function to shuffle the data and then finding the auto correlation 
 def shuff_acf(data):
     np.random.shuffle(data)
     shuff_corr = acf(data,fft=True)
     return shuff_corr
 
+# Smoothing data by convolution
 def smooth(y, box_pts=3):
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='same')
