b719e4b121e2ad35f23d91fce1f324a5d29694cb
nosratullah <nosratullah.mohammadi@gmail.com>
2020-05-09 14:01:27 +0430

    !

--- lfp_plotly.py
+++ lfp_plotly.py
@@ -13,29 +13,35 @@ times = np.array(data['time'][100:])
 #ex_lfp = lc.smooth(ex_lfp,5)
 #in_lfp = lc.smooth(in_lfp,5)
 # The Furiertransform Part
-ex_fft, time_domain = lc.manualFFT(ex_lfp,time)
-in_fft, time_domain = lc.manualFFT(in_lfp,time)
+ex_fft, time_domain = manualFFT(ex_lfp)
+in_fft, time_domain = manualFFT(in_lfp)
+ex_fft = ex_fft[:int(len(ex_fft)/2)]
+in_fft = in_fft[:int(len(in_fft)/2)]
+time_domain = time_domain[:int(len(time_domain)/2)]
 # The Plotting Part
 
-fig = make_subplots(rows=2, cols=2, shared_xaxes=True)
+fig = make_subplots(rows=2, cols=2, shared_xaxes=True,
+                    subplot_titles=("excitatory lfp", "excitatory fft", "inhibitory lfp", "inhibitory fft"))
 fig.add_trace(go.Scatter(x=times, y=ex_lfp,
                     mode='lines',
                     name='excitatory lfp'),
              row=1, col=1)
 fig.add_trace(go.Scatter(x=time_domain, y=ex_fft,
                     mode='lines',
-                    name='excitatory lfp'),
-             row=2, col=1)
+                    name='excitatory fft'),
+             row=1, col=2)
 fig.add_trace(go.Scatter(x=times, y=in_lfp,
                     mode='lines',
                     name='inhibitory lfp'),
-             row=1, col=2)
+             row=2, col=1)
 fig.add_trace(go.Scatter(x=time_domain, y=in_fft,
                     mode='lines',
-                    name='inhibitory lfp'),
+                    name='inhibitory fft'),
              row=2, col=2)
 
-#fig.add_trace(go.Scatter(x=random_x, y=random_y2,
-#                   mode='markers', name='markers'))
+fig.update_yaxes(title_text="Voltage (mV)", row=1, col=1)
+fig.update_yaxes(title_text="Intensity", row=1, col=2)
+fig.update_yaxes(title_text="Voltage (mV)", row=2, col=1)
+fig.update_yaxes(title_text="Intensity", row=2, col=2)
 
-#fig.show()
+fig.show()
